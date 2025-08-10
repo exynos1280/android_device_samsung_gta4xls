c9b54849ba4435a9c3e7b64c1a42b0d60dc3eea7
Majaahh <maja.blaszczak@proton.me>
2025-08-10 17:59:11 +0200

    gta4xls: Init
    Change-Id: Ia36624aa2aa65bf192bc79228a075ebf1b73bc1d
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ extract-files.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/samsung/s5e8825-common',
+    'hardware/samsung',
+    'hardware/samsung_slsi-linaro/exynos',
+    'hardware/samsung_slsi-linaro/graphics',
+    'hardware/samsung_slsi-linaro/interfaces',
+    'vendor/samsung/s5e8825-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'gta4xls',
+    'samsung',
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
+    blob_fixups=blob_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 's5e8825-common', module.vendor
+    )
+    utils.run()
